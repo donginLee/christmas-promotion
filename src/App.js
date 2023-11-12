1e58940512564d0500f77ccf6fe87fcef4dc87f0
donginLee <55528388+donginLee@users.noreply.github.com>
2023-11-12 19:43:58 +0900

    feat: App.js에서 날짜 인풋 받는 기능 구현
    retryUntilNoError 함수, readDate 함수, Date 클래스 활용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import InputView from './InputView.js';
 import OutputView from './OutputView.js';
+
+import Date from './Date.js';
+
+import { Console } from '@woowacourse/mission-utils';
+import retryUntilNoError from './RetryUntilNoError.js';
 class App {
   async run() {
     OutputView.introduction();
+    retryUntilNoError(async ()=>{
+      const dateInput = await InputView.readDate();
+      const date = new Date(dateInput);
+    });
   }
 }
 
