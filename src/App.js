68555a31af3f69cddfc0f934e06145ca4253a57a
donginLee <55528388+donginLee@users.noreply.github.com>
2023-11-15 16:49:00 +0900

    fix: 공백 출력 수정

--- src/App.js
+++ src/App.js
@@ -25,21 +25,13 @@ class App {
     });
 
     OutputView.showBenefitPreviewTitle();
-
     OutputView.showOrder('주문 메뉴',bill.order.ORDER);
-
     OutputView.showTotalOrderAmountBeforeDiscount('할인 전 총주문 금액',bill.totalOrderAmountBeforeDiscount);
-
     OutputView.showGiftMenu('증정 메뉴', bill);
-    
     OutputView.showDiscountHistory('혜택 내역', bill.totalDiscount, bill.discountHistory);
-    
     OutputView.showTotalDiscount('총혜택 금액', bill.totalBenefit);
-
     OutputView.showFinalPaymentAmount('할인 후 예상 결제 금액', bill.finalPaymentAmount);
-
     OutputView.showDecemberPromotionBadge('12월 이벤트 배지', bill.decemberPromotionBadge);
-
   }
 }
 
--- src/OutputView.js
+++ src/OutputView.js
@@ -7,7 +7,7 @@ const OutputView = {
     // ...
   },
   showBenefitPreviewTitle() {
-    Console.print('12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!');
+    Console.print('12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n');
   },
   showOrder(title,order) {
     Console.print(wrapTitle(title));
@@ -15,15 +15,14 @@ const OutputView = {
       const count = order[menu];
       Console.print(`${menu} ${count}개`);
     });
-    Console.print('\n');
   },
   showTotalOrderAmountBeforeDiscount(title, number) {
-    Console.print(wrapTitle(title));
+    Console.print(`\n${wrapTitle(title)}`);
     Console.print(formatCurrency(number));
-    Console.print('\n');
+
   },
   showGiftMenu(title, bill){
-    Console.print(wrapTitle(title));
+    Console.print(`\n${wrapTitle(title)}`);
     if(bill.willProvideGift) {
       Object.keys(bill.Gift).forEach( menu => {
         const count = bill.Gift[menu];
@@ -34,26 +33,27 @@ const OutputView = {
     Console.print('없음');
   },
   showDiscountHistory(title, totalDiscount, discountHistory) {
-    Console.print(wrapTitle(title));
-    if(totalDiscount === 0) {Console.print('없음'); return;}
+    Console.print(`\n${wrapTitle(title)}`);
+    if(totalDiscount === 0) {Console.print('없음\n');return;}
     Object.keys(discountHistory).forEach( title => {
       Console.print(`${title}: -${formatCurrency(discountHistory[title])}`);
     });
   },
   showTotalDiscount(title,totalDiscount) {
-    Console.print(wrapTitle(title));
-    Console.print(`-${formatCurrency(totalDiscount)}`);
-    Console.print('\n');
+    Console.print(`\n${wrapTitle(title)}`);
+    if(totalDiscount === 0){
+      Console.print(`${formatCurrency(totalDiscount)}\n`);
+      return;
+    }
+    Console.print(`-${formatCurrency(totalDiscount)}\n`);
   },
   showFinalPaymentAmount(title,finalPaymentAmount) {
     Console.print(wrapTitle(title));
-    Console.print(`${formatCurrency(finalPaymentAmount)}`);
-    Console.print('\n');
+    Console.print(`${formatCurrency(finalPaymentAmount)}\n`);
   },
   showDecemberPromotionBadge(title, decemberPromotionBadge) {
     Console.print(wrapTitle(title));
     Console.print(decemberPromotionBadge);
-    Console.print('\n');
   }
   // ...
 };
